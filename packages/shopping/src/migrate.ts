79a09fe41a0c1393f5536618f1e97fef497a272c
guxukai <1378026744@qq.com>
2020-01-19 17:38:53 +0800

    feat: category;

--- packages/shopping/src/migrate.ts
+++ packages/shopping/src/migrate.ts
@@ -1,12 +1,23 @@
 import {ShoppingApplication} from './application';
-
+import { CategoryRepository } from './repositories'
+import { Category } from './models'
 export async function migrate(args: string[]) {
   const existingSchema = args.includes('--rebuild') ? 'drop' : 'alter';
   console.log('Migrating schemas (%s existing schema)', existingSchema);
-
   const app = new ShoppingApplication();
   await app.boot();
   await app.migrateSchema({existingSchema});
+  const categoryRepo = await app.getRepository(CategoryRepository);
+  const categories = [{name: 'NodeJs'}, {name: 'Linux'}]
+  categories.map( async  e => {
+    const found = await categoryRepo.findOne({where: e})
+    if(found) {
+      await categoryRepo.updateById(found.id, new Category(e))
+    } else {
+      const category: Omit<Category, 'id'> = new Category(e)
+      await categoryRepo.create(category)
+    }
+  })
 
   // Connectors usually keep a pool of opened connections,
   // this keeps the process running even after all work is done.
